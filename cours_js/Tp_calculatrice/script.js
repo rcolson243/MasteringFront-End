bb293b468ff44c25dcc9634c67537cb5524b1990
colson raphael <raphael.kasese@gmail.com>
2022-05-28 16:16:13 +0200

    js les base

---
+++ cours_js/Tp_calculatrice/script.js
@@ -0,0 +1,35 @@
+let total = 0;
+
+function addition(x) {
+    total += x;
+    return total;
+}
+
+function soustraction(x) {
+    total -= x;
+    return total;
+}
+
+function miltiplication(x) {
+    if (total === 0) {
+        return (total = x)
+    }else {
+        total *= x;
+        return total;
+    }
+
+}
+
+function division(x) {
+    if (total === 0) {
+        return (total = x)
+    }else {
+        total /= x;
+    return total;
+    }
+}
+
+function reset() {
+    total = 0;
+    return total;
+}
